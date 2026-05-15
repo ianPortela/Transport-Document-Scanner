08e890489ac55cb99e8d645a2ee8a934bcf70377
Ian Portela Miranda <ian080406@gmail.com>
2026-05-15 14:28:25 -0300

    se paso el tipo de documento a ManualLoadingScreen y se uso para validar el Id dependiendo del tipo de documento

--- app/src/main/java/com/example/transportdocumentscanner/ui/Navigation/NavigationWarapped.kt
+++ app/src/main/java/com/example/transportdocumentscanner/ui/Navigation/NavigationWarapped.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.example.transportdocumentscanner.ui.Presentation.Views.HomeScreen
 import com.example.transportdocumentscanner.ui.Presentation.Views.ManualLoadingScreen
 
@@ -17,11 +18,13 @@ fun NavigationWarapped() {
     NavHost(navController = navController, startDestination = Home) {
         composable<Home> {
             HomeScreen(
-                navigateToManualLoading = { navController.navigate(LoadingScreen) }
+                navigateToManualLoading = {typeDoc -> navController.navigate(LoadingScreen(typeDoc = typeDoc)) }
             )
         }
-        composable<LoadingScreen> {
+        composable<LoadingScreen> {backStackEntry ->
+            val loadingScreen = backStackEntry.toRoute<LoadingScreen>()
             ManualLoadingScreen(
+                loadingScreen.typeDoc,
                 navigateToHome = { navController.navigate(Home) }
             )
         }
