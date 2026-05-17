ef0e9a6b3f37d87f756bd690870e5249dd72dabf
Ian Portela Miranda <ian080406@gmail.com>
2026-05-17 17:24:11 -0300

    creacion de la vista scanner para usar la camara

--- app/src/main/java/com/example/transportdocumentscanner/ui/Presentation/Views/ScannerScreen.kt
+++ app/src/main/java/com/example/transportdocumentscanner/ui/Presentation/Views/ScannerScreen.kt
@@ -1,4 +1,48 @@
 package com.example.transportdocumentscanner.ui.Presentation.Views
 
-class ScannerScreen {
+import android.Manifest
+import android.view.ViewGroup
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun ScannerScreen() {
+    val permissionState = rememberPermissionState(permission = Manifest.permission.CAMERA)
+    LaunchedEffect(Unit) {
+        permissionState.launchPermissionRequest()
+    }
+
+    if(permissionState.status.isGranted){
+        val context = LocalContext.current
+        val cameraController = remember {
+            LifecycleCameraController(context)
+        }
+
+        val lifecycle = LocalLifecycleOwner.current
+        cameraController.bindToLifecycle(lifecycle)
+
+        AndroidView({context ->
+            val previewView = PreviewView(context).apply{
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            }
+            previewView.controller = cameraController
+
+            previewView
+        })
+    }else{
+
+    }
 }
